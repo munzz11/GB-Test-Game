fc571b2e834fe447417b8204774b2cdcf1080123
Avery Munoz <munoza@wit,edu>
2021-02-10 16:00:15 -0500

    Updated init()
    moved sound and screen initialization to init() also moved tile and background init, may move back later depending on number of separate tile maps I end up having.

--- main.c
+++ main.c
@@ -5,14 +5,24 @@
 
 void main()
 {
-   set_bkg_data(0,10, BackgroundTileSet);
-   set_bkg_tiles(0,0,BackgroundTileMapWidth, BackgroundTileMapHeight, BackgroundTileMap);
+    init();
 
-   SHOW_BKG;                                        //show background map   
-   DISPLAY_ON;                                      //turn on display
    while(1)
    {
        scroll_bkg(1,0);                          //scroll in the y axis
        wait_vbl_done();                          //wait for screen update
    }
 }
+
+void init() 
+{
+    set_bkg_data(0,10, BackgroundTileSet);                                                    //Initialize tile set
+    set_bkg_tiles(0,0,BackgroundTileMapWidth, BackgroundTileMapHeight, BackgroundTileMap);    //Initialize tile map
+
+    SHOW_BKG;
+	DISPLAY_ON;		// Turn on the display
+	NR52_REG = 0x8F;	// Turn on the sound
+	NR51_REG = 0x11;	// Enable the sound channels
+	NR50_REG = 0x77;	// Increase the volume to its max
+}
+
